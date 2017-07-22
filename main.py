e61acf213cb2be0336c49ad775211318fa02283d
Jim Vargas <jimball1@hotmail.com>
2017-07-22 03:56:30 -0700

    Register and some minor other changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'poop'
@@ -14,11 +14,24 @@ app.secret_key = 'poop'
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(500))
+    body = db.Column(db.String(5000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route("/")
@@ -69,6 +82,44 @@ def OneBlog():
 
     return render_template("individual.html", 
         title= existing_blog.title, body= existing_blog.body, id= existing_blog.id)
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    error = {"name_error": "", "pass_error": "", "verify_error": ""}
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if username == "":
+            error["name_error"] = "Username cannot be blank"
+        if password == "":
+            error["pass_error"] = "Password cannot be blank"
+        elif len(password) < 2:
+            error["pass_error"] = "Password must be more than two characters long"
+        else:
+            if password != verify:
+                error["verify_error"] = "Pasword and Verify must match"
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            error["name_error"] = "There is already somebody with that username"
+
+        if error["name_error"] == "" and error["pass_error"] == "" and error["verify_error"] == "":
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/add")
+        else:
+            return render_template('register.html', title= "Register for this Blog", 
+                name_error= error["name_error"], pass_error= error["pass_error"],
+                verify_error= error["verify_error"])
+
+
+    return render_template("register.html", title= "Register for this Blog",
+        name_error= error["name_error"], pass_error= error["pass_error"],
+        verify_error= error["verify_error"])
 
 
 if __name__ == '__main__':
